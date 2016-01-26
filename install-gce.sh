fc99b4e618149703dd685944e577c9280d90699b
Jean-Baptiste Claramonte <jbclaramonte@xebia.fr>
2016-01-26 21:20:52 +0100

    shell install

--- install-gce.sh
+++ install-gce.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-sudo apt-get update
 sudo apt-get install -y unzip
-sudo apt-get install -y git
 install nodejs npm
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
