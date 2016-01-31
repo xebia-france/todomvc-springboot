e939d9df236abc55025b119c7b375cbcfbf98c50
Jean-Baptiste Claramonte <jbclaramonte@xebia.fr>
2016-01-31 18:20:01 +0100

    keeping only the redis api in the master branch

--- api/Config.groovy
+++ api/Config.groovy
@@ -6,7 +6,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 @Grab(group='org.springframework.boot', module='spring-boot-starter-redis', version='1.3.2.RELEASE')
 @Grab(group='io.springfox', module='springfox-swagger2', version='2.3.1')
 @Grab(group='io.springfox', module='springfox-swagger-ui', version='2.3.1')
-@Grab(group='mysql', module='mysql-connector-java', version='5.1.38')
 
 
 @EnableSwagger2
