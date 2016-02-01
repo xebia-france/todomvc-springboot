de5f6ab675ebeba095078454b090ac371c29ca86
Jean-Baptiste Claramonte <jbclaramonte@xebia.fr>
2016-02-01 09:34:16 +0100

    fix install-gce.sh

--- install-gce.sh
+++ install-gce.sh
@@ -5,8 +5,8 @@ curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
 cd ~/todomvc-springboot/static
 npm install
-install spring cli
+# install spring cli
 sudo apt-get install -y openjdk-8-jdk
 curl -s http://get.sdkman.io | bash
-source "/home/jbclaramonte/.sdkman/bin/sdkman-init.sh"
+source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install springboot
